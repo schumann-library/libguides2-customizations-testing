3bf5bd92bd540f90af6215c113a9743975c7b044
Quinn Montgomery <quinninthetardis@gmail.com>
2022-12-01 15:26:36 -0500

    Code cleanup

--- javascripts/hamburger-menu-navbar.js
+++ javascripts/hamburger-menu-navbar.js
@@ -6,23 +6,16 @@ $(document).ready(function () {
 
     });
 
-    // $(window).resize(function () {
-    //     if(document.body.clientWidth <= 810){
-    //         $(".in").css("display", "none")
-    //     } else {
-    //         $(".in").css("display", "unset")
-    //     }
-    // })
-
     $('.dropdown-toggle').on('click', function () {
+        
         $(this).parent().parent().toggleClass('move');
-        // console.log(["translate(100%, ", document.body.clientHeight - getOffset($(this)).top, "px", ")"].join(""));
-        // $(".dropdown-menu").css("transform", ["translate(100%, ", document.body.clientHeight - getOffset($(".dropdown-menu")).top, "px", ")"].join(""))
+
     });
     
     $('.return-lvl').on('click', function () {
+
         $(this).parent().parent().parent().parent().removeClass('move');
-        
+
     });
 
 });
